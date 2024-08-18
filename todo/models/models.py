17ffa1a81df3122cce0d999afa11587fc58bd3ed
ivan ivaneev <ivaneev.ivane@gmail.com>
2024-08-18 21:39:05 +0300

    fix models

--- todo/models/models.py
+++ todo/models/models.py
@@ -14,21 +14,6 @@ class Role(Base):
     users: Mapped[List["User"]] = relationship(back_populates="role")
 
 
-class User(Base):
-    __tablename__ = 'user'
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(30), default=False, index=True)
-    email = Column(String(50), default=False, index=True)
-    password = Column(String(30), default=False, index=True)
-    role = relationship(Role, back_populates='users')
-    role_id = Column(Integer, ForeignKey("role.id"))
-    tasks: Mapped[List["Task"]] = relationship(back_populates="executor")
-    created_tasks: Mapped[List["Task"]] = relationship(back_populates="author")
-    comments: Mapped[List["Comment"]] = relationship(back_populates="author")
-    create_at = Column(DateTime, default=func.now())
-    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
-
-
 class Dashboard(Base):
     __tablename__ = 'dashboard'
     id = Column(Integer, primary_key=True, index=True)
@@ -41,6 +26,21 @@ class Status(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(225), index=True)
     tasks: Mapped[List["Task"]] = relationship(back_populates="status")
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(30), default=False, index=True)
+    email = Column(String(50), default=False, index=True)
+    password = Column(String(30), default=False, index=True)
+    role = relationship(Role, back_populates='users')
+    role_id = Column(Integer, ForeignKey("role.id"))
+    tasks: Mapped[List["Task"]] = relationship("Task", back_populates="executor", foreign_keys="[Task.executor_id]")
+    created_tasks: Mapped[List["Task"]] = relationship("Task", back_populates="author", foreign_keys="[Task.author_id]")
+    comments: Mapped[List["Comment"]] = relationship(back_populates="author")
+    create_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
 
 
 class Task(Base):
@@ -56,10 +56,13 @@ class Task(Base):
     parent_id = Column(Integer, ForeignKey('task.id'))
     parent = relationship('Task', remote_side=[id])
     tasks: Mapped[List["Task"]] = relationship(back_populates="parent")
-    executor = relationship(User, back_populates='tasks')
+
     executor_id = Column(Integer, ForeignKey('user.id'))
-    author = relationship(User, back_populates='created_tasks')
     author_id = Column(Integer, ForeignKey('user.id'))
+
+    executor = relationship("User", back_populates='tasks', foreign_keys=[executor_id])
+    author = relationship("User", back_populates='created_tasks', foreign_keys=[author_id])
+
     create_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
 
